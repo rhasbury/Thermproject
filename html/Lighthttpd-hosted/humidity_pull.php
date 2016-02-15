77e64bdb2703d573a49b7df5aa98643862307541
rhasbury <rhasbury@gmail.com>
2016-02-15 17:09:39 -0500

    Placed the current graphs in the http folder

---
+++ html/Lighthttpd-hosted/humidity_pull.php
@@ -0,0 +1,33 @@
+<?php
+    $username = "monitor"; 
+    $password = "password";   
+    $host = "localhost";
+    $database="temps";
+    
+    $server = mysql_connect($host, $username, $password);
+    $connection = mysql_select_db($database, $server);
+
+
+    $myquery = "
+SELECT  UNIX_TIMESTAMP(`tdate`) AS 'fix_time', zone, humidity  FROM  humdat ORDER by tdate DESC LIMIT 3000
+";
+
+
+	error_log($myquery , 0);
+    $query = mysql_query($myquery);
+    
+    if ( ! $query ) {
+        echo mysql_error();
+        die;
+    }
+    
+    $data = array();
+    
+    for ($x = 0; $x < mysql_num_rows($query); $x++) {
+        $data[] = mysql_fetch_assoc($query);
+    }
+    
+    echo json_encode($data);     
+     
+    mysql_close($server);
+?>
