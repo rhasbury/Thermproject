78074b42f2960ac8d180546ab02717f3656988e9
Ryan Hasbury <rhasbury@gmail.com>
2020-11-17 20:00:45 -0500

    Added timeout to remote sensor read

--- python/script.py
+++ python/script.py
@@ -503,7 +503,8 @@ class updateTemps(threading.Thread):
                         my_logger.debug("Reading {}".format(key))                
                         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
                             # Connect to server and send data
-                            sock.connect((HOST, PORT))
+                            sock.settimeout(5)
+							sock.connect((HOST, PORT))							
                             sock.sendall(bytes("get_temp\n", "utf-8"))    
                             # Receive data from the server and shut down
                             received = str(sock.recv(1024), "utf-8")
